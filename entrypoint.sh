b5df6ad8acd5b0faf1c44db1c02b108ff086799a
Oleg M <youwillnotfindme@gmail.com>
2021-06-07 17:53:08 +0200

    tests

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,13 +3,6 @@
 set -e
 
 export MONGO_URI=${MONGO_URI:-mongodb://mongo:27017}
-export TARGET_FOLDER=${TARGET_FOLDER-/backup}   # can be set to null
-
-# Optional env vars:
-# - CRON_SCHEDULE
-# - TARGET_CONTAINER
-# - AWS_ACCESS_KEY_ID
-# - AWS_SECRET_ACCESS_KEY
 
 if [[ "$CRON_SCHEDULE" ]]; then
     LOGFIFO='/var/log/cron.fifo'
@@ -17,17 +10,8 @@ if [[ "$CRON_SCHEDULE" ]]; then
         mkfifo "$LOGFIFO"
     fi
     CRON_ENV="MONGO_URI='$MONGO_URI'"
-    if [[ "$TARGET_FOLDER" ]]; then
-        CRON_ENV="$CRON_ENV\nTARGET_FOLDER='$TARGET_FOLDER'"
-    fi
     if [[ "$TARGET_CONTAINER" ]]; then
         CRON_ENV="$CRON_ENV\nTARGET_CONTAINER='$TARGET_CONTAINER'"
-    fi
-    if [[ "$AWS_ACCESS_KEY_ID" ]]; then
-        CRON_ENV="$CRON_ENV\nAWS_ACCESS_KEY_ID='$AWS_ACCESS_KEY_ID'"
-    fi
-    if [[ "$AWS_SECRET_ACCESS_KEY" ]]; then
-        CRON_ENV="$CRON_ENV\nAWS_SECRET_ACCESS_KEY='$AWS_SECRET_ACCESS_KEY'"
     fi
     echo -e "$CRON_ENV\n$CRON_SCHEDULE /backup.sh > $LOGFIFO 2>&1" | crontab -
     crontab -l
